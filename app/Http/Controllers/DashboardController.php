16d482caabc4b72de43d453a9864e5acba6bb01a
FriedRice <hello@FriedRice.com>
2025-10-16 10:39:39 +0700

    p

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\DB; // <-- Jangan lupa import DB
+use Illuminate\Support\Facades\DB;
 
 class DashboardController extends Controller
 {
